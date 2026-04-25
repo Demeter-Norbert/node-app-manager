3de04bc19f59be56c7b75cbf377162e9be3c2be8
Demeter Norbert <demeter.m.norbert@student.ms.sapientia.ro>
2026-04-25 21:43:11 +0300

    ADD docker event listener for unexpected container exit detection

---
+++ backend/app/core/events.py
@@ -0,0 +1,34 @@
+import asyncio
+from app.core.manager import NodeAppManager
+from app.utils.log_analyzer import extract_nodejs_error
+from app.services.notifier import send_ntfy_alert
+
+manager = NodeAppManager()
+
+def docker_event_listener():
+    try:
+        print("Docker event listener started ...")
+        for event in manager.client.events(decode=True, filters={"type": "container", "event": "die"}):
+            attributes = event.get("Actor", {}).get("Attributes", {})
+            container_id = event.get("id")
+            
+            if attributes.get("node-manager") == "managed":
+                container_name = attributes.get("name", "Unknown")
+                exit_code = attributes.get("exitCode", "0")
+                
+                if exit_code not in ["0", "137", "143"]:
+                    error_log = "Hiba a log lekérésekor."
+                    try:
+                        container = manager.client.containers.get(container_id)
+                        
+                        logs_bytes = container.logs(tail=200)
+                        logs_str = logs_bytes.decode('utf-8').strip()
+                        
+                        error_log = extract_nodejs_error(logs_str)
+                            
+                    except Exception as log_err:
+                        error_log = f"[Log olvasási hiba: {log_err}]"
+
+                    asyncio.run(send_ntfy_alert(container_name, exit_code, error_log))
+    except Exception as e:
+        print(f"Critical event listener error: {e}")
